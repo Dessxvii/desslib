125a04c7fbd27701ecf492d1b855f2a524c8939f
Destiny ifeanyi <122130140+Dessxvii@users.noreply.github.com>
2023-04-21 12:25:14 +0100

    Update xvii.js

--- xvii.js
+++ xvii.js
@@ -7,6 +7,6 @@
 
 
 
-const { _0x993 , _0x369 } = require('./_0x000/_0x00')
+const { keepAlive , init } = require('./core/base')
 
-module.exports = { _0x993 as keepAlive, _0x369 as init}
+module.exports = { keepAlive, init}
